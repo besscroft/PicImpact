40b67076ae11e352ddb5ea669c88eb20b3717459
besscroft <besscroft@foxmail.com>
2024-04-03 17:11:06 +0800

    feat: add useBreakPoints

---
+++ utils/useBreakpoints.ts
@@ -0,0 +1,18 @@
+import { useMediaQuery } from 'react-responsive';
+import { breakpointsTailwind } from '~/constants/breakpoints';
+
+export function useBreakpoints() {
+    const smAndLarger  = useMediaQuery({ query: `(min-width: ${breakpointsTailwind.sm}px)` })
+    const mdAndLarger = useMediaQuery({ query: `(min-width: ${breakpointsTailwind.md}px)` })
+    const lgAndLarger = useMediaQuery({ query: `(min-width: ${breakpointsTailwind.lg}px)` })
+    const xlAndLarger = useMediaQuery({ query: `(min-width: ${breakpointsTailwind.xl}px)` })
+    const twoXlAndLarger = useMediaQuery({ query: `(min-width: ${breakpointsTailwind["2xl"]}px)` })
+
+    return {
+        smAndLarger,
+        mdAndLarger,
+        lgAndLarger,
+        xlAndLarger,
+        twoXlAndLarger
+    }
+}
