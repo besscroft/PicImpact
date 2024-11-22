f8dcd17ba554b3c5377d03864fb44cc4a0296504
besscroft <besscroft@foxmail.com>
2024-11-22 13:30:58 +0800

    fix: 修复 livephoto 无法更新问题

--- components/admin/list/ImageEditSheet.tsx
+++ components/admin/list/ImageEditSheet.tsx
@@ -121,6 +121,21 @@ export default function ImageEditSheet(props : Readonly<ImageServerHandleProps &
             />
           </label>
           <label
+            htmlFor="video_url"
+            className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <span className="text-xs font-medium text-gray-700"> 视频链接 </span>
+
+            <input
+              type="text"
+              id="video_url"
+              placeholder="输入视频链接"
+              value={image?.video_url}
+              onChange={(e) => setImageEditData({...image, video_url: e.target.value})}
+              className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+          </label>
+          <label
             htmlFor="detail"
             className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
           >
