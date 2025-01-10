ac746f4142fa2d3704781102850a78f9339c9631
besscroft <besscroft@foxmail.com>
2025-01-10 17:32:01 +0800

    fix: add translate

--- components/admin/copyright/CopyrightList.tsx
+++ components/admin/copyright/CopyrightList.tsx
@@ -5,11 +5,6 @@ import { HandleProps, CopyrightType } from '~/types'
 import { useSWRHydrated } from '~/hooks/useSWRHydrated'
 import { toast } from 'sonner'
 import { useButtonStore } from '~/app/providers/button-store-Providers'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '~/components/ui/popover'
 import { Switch } from '~/components/ui/switch'
 import { Button } from '~/components/ui/button'
 import {
@@ -24,6 +19,7 @@ import { ReloadIcon } from '@radix-ui/react-icons'
 import { Card, CardFooter } from '~/components/ui/card'
 import { SquarePenIcon } from '~/components/icons/square-pen'
 import { DeleteIcon } from '~/components/icons/delete'
+import { useTranslations } from 'next-intl'
 
 export default function CopyrightList(props : Readonly<HandleProps>) {
   const { data, isLoading, error, mutate } = useSWRHydrated(props)
@@ -34,6 +30,7 @@ export default function CopyrightList(props : Readonly<HandleProps>) {
   const { setCopyrightEdit, setCopyrightEditData } = useButtonStore(
     (state) => state,
   )
+  const t = useTranslations()
 
   async function deleteCopyright() {
     setDeleteLoading(true)
@@ -89,17 +86,7 @@ export default function CopyrightList(props : Readonly<HandleProps>) {
         <Card key={copyright.id} className="flex flex-col h-72 show-up-motion items-center">
           <div className="flex h-12 justify-start w-full p-2 space-x-2">
             <p>{copyright.name}</p>
-            <Popover>
-              <PopoverTrigger className="cursor-pointer select-none inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700">
-                <p className="whitespace-nowrap text-sm">{copyright.type === 'social' ? '社交媒体' : '外链'}</p>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div className="px-1 py-2 select-none">
-                  <div className="text-small font-bold">类型</div>
-                  <div className="text-tiny">版权声明类型</div>
-                </div>
-              </PopoverContent>
-            </Popover>
+            <p className="select-none inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 whitespace-nowrap text-sm">{copyright.type === 'social' ? t('Copyright.social') : t('Copyright.external')}</p>
           </div>
           <div className="flex justify-start w-full p-2 h-48">{copyright.detail || 'N&A'}</div>
           <CardFooter className="flex h-12 p-2 mb-1 space-x-1 select-none before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl rounded-large w-[calc(100%_-_8px)] shadow-small z-10">
@@ -116,7 +103,7 @@ export default function CopyrightList(props : Readonly<HandleProps>) {
               {
                 copyright.default === 0 &&
                 <span className="inline-flex items-center justify-center rounded-full border border-emerald-500 px-2.5 py-0.5 text-emerald-700">
-                  <p className="whitespace-nowrap text-sm">默认</p>
+                  <p className="whitespace-nowrap text-sm">{t('Words.default')}</p>
                 </span>
               }
             </div>
@@ -128,7 +115,7 @@ export default function CopyrightList(props : Readonly<HandleProps>) {
                   setCopyrightEditData(copyright)
                   setCopyrightEdit(true)
                 }}
-                aria-label="编辑版权"
+                aria-label={t('Copyright.editCopyright')}
               >
                 <SquarePenIcon />
               </Button>
@@ -144,27 +131,27 @@ export default function CopyrightList(props : Readonly<HandleProps>) {
                     onClick={() => {
                       setCopyright(copyright)
                     }}
-                    aria-label="删除版权"
+                    aria-label={t('Copyright.deleteCopyright')}
                   >
                     <DeleteIcon />
                   </Button>
                 </DialogTrigger>
                 <DialogContent className="sm:max-w-[425px]">
                   <DialogHeader>
-                    <DialogTitle>确定要删掉？</DialogTitle>
+                    <DialogTitle>{t('Tips.reallyDelete')}</DialogTitle>
                   </DialogHeader>
                   <div>
-                    <p>版权 ID：{copyright.id}</p>
-                    <p>版权名称：{copyright.name}</p>
+                    <p>{t('Copyright.copyrightId')}：{copyright.id}</p>
+                    <p>{t('Copyright.copyrightName')}：{copyright.name}</p>
                   </div>
                   <DialogFooter>
                     <Button
                       disabled={deleteLoading}
                       onClick={() => deleteCopyright()}
-                      aria-label="确认删除"
+                      aria-label={t('Button.yesDelete')}
                     >
                       {deleteLoading && <ReloadIcon className="mr-2 h-4 w-4 animate-spin"/>}
-                      删除
+                      {t('Button.delete')}
                     </Button>
                   </DialogFooter>
                 </DialogContent>
