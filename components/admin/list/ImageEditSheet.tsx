6bc64b5e8393f4521c0b3d6b83d6675bc11b21f5
besscroft <besscroft@foxmail.com>
2025-04-08 21:45:28 +0800

    style: add cursor-pointer

--- components/admin/list/ImageEditSheet.tsx
+++ components/admin/list/ImageEditSheet.tsx
@@ -287,6 +287,7 @@ export default function ImageEditSheet(props : Readonly<ImageServerHandleProps &
             />
           </div>
           <Button
+            className="cursor-pointer"
             disabled={loading}
             onClick={() => submit()}
             aria-label="更新"
