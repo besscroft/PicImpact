074b44f53f8d6ca4aa14410827e0b92d4acb8d9f
besscroft <besscroft@foxmail.com>
2025-04-25 23:17:43 +0800

    fix: eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next', 'prettier', 'next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      'quotes': ['error', 'single'],
+      '@typescript-eslint/no-explicit-any': 'warn'
+    },
+  }),
 ];
 
 export default eslintConfig;
